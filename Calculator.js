e4569d7d27b2966d0478704e04bc79c2a8188a26
Lakshyash5820 <163559789+Lakshyash5820@users.noreply.github.com>
2026-04-30 07:13:17 +0000

    no message

--- Calculator.js
+++ Calculator.js
@@ -2,7 +2,14 @@ var i = document.querySelector("#dis")
 i.value = "";
 var t1 = document.querySelector("#t1")
 t1.addEventListener("click", function(e){
+
     if(e.target.classList.contains("btn")){
+
+        if(i.value.match(/[a-zA-Z]/)){
+            alert("Please enter a valid number");
+                i.value = "";
+        }
+       else{ 
         if(e.target.innerText === "Ac"){
             i.value = "";
         }
@@ -12,5 +19,6 @@ t1.addEventListener("click", function(e){
         else{
             i.value=  i.value + e.target.innerText;
         }
-    }
+        }
+    }    
 });
